c94b1c87ab3a303b691d45191b90789261eca6eb
fatihbguclu <curaat79@gmail.com>
2023-08-17 16:02:18 +0300

    initial commit

---
+++ twitter-to-kafka-service/src/test/java/com/ft/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.twittertokafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
