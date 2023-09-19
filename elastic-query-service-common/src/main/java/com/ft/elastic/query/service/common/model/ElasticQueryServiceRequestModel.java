377b7f0b0845264bfcf57dfde44fc194d89c0511
fatihbguclu <curaat79@gmail.com>
2023-09-19 15:08:08 +0300

    add common lib for elastic query service

---
+++ elastic-query-service-common/src/main/java/com/ft/elastic/query/service/common/model/ElasticQueryServiceRequestModel.java
@@ -0,0 +1,19 @@
+package com.ft.elastic.query.service.common.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryServiceRequestModel {
+    private String id;
+    @NotEmpty
+    private String text;
+}
